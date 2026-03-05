35e1d417a0ff33e310611fbcb179fcedc5e213b3
mar-tor-e <marta.marrone2@studio.unibo.it>
2026-03-05 15:00:16 +0100

    Update 02_visualization.R

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -41,3 +41,31 @@ plot(b2, col=cl)
 im.multiframe(1,2)
 plot(b2,col=inferno(100))
 plot(b2, col=cl)
+
+#importare la terza banda
+im.list()
+#per vedere la lista di tutte le bande di sentinel
+b3= im.import("sentinel.dolomites.b3.tif")
+#tutte le componenti che riflettono molto il verde in questa banda diventano gialle
+#cambiare palette:
+plot(b3, col=plasma(100))
+
+#banda 4 e banda 8
+b4 = im.import("sentinel.dolomites.b4.tif")
+b8= im.import("sentinel.dolomites.b8.tif") 
+
+#esercizio: multiframe con tutte e 4 le bande e con le legende 
+par(mfrow=c(2,2)
+cl=colorRampPalette(c("lightblue","blue","darkblue"))(100)
+plot(b2, col=cl)
+cl=colorRampPalette(c("lightgreen","green","darkgreen"))(100)
+plot(b3, col=cl)
+cl=colorRampPalette(c("pink","red","darkred"))(100)
+plot(b4, col=cl)
+cl=colorRampPalette(c("salmon1","salmon3","salmon4"))(100)
+plot(b8, col=cl)
+
+plot(b2, col=inferno(100))
+plot(b3, col=inferno(100))
+plot(b4, col=inferno(100))
+plot(b8, col=inferno(100))
